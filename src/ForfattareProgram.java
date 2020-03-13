16033a5793e119cf4b178e48ef3d67df9e255af8
AUTISM81 <54669344+AUTISM81@users.noreply.github.com>
2020-03-13 11:03:01 +0100

    Trying to do save function, JFilechooser started using. Trying to make you choose destination for save file

--- src/ForfattareProgram.java
+++ src/ForfattareProgram.java
@@ -1,3 +1,5 @@
+
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -8,30 +10,33 @@ import java.nio.file.Paths;
 public class ForfattareProgram {
     private JPanel panel1;
     private JTextPane textPane1;
+    private JMenuItem save;
+    private JMenuItem open;
+    private JFrame frame;
 
-
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("ForfattareProgram");
-        frame.setJMenuBar(menuBar());
-
-        frame.setContentPane(new ForfattareProgram().panel1);
+    public ForfattareProgram() {
+        frame = new JFrame("ForfattareProgram");
+        frame.setContentPane(this.panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+        frame.setJMenuBar(menuBar());
+        save.addActionListener(new saveTxt());
+    }
+
+    public static void main(String[] args) {
+        new ForfattareProgram();
 
     }
 
-    public static JMenuBar menuBar() {
+    public JMenuBar menuBar() {
         JMenuBar menuBar;
         JMenu menu;
-        JMenuItem save;
-        JMenuItem open;
 
         menuBar = new JMenuBar();
         menu = new JMenu("Menu");
         save = new JMenuItem("save");
         open = new JMenuItem("open");
-
 
         menuBar.add(menu);
         menu.add(save);
@@ -39,15 +44,34 @@ public class ForfattareProgram {
 
         return menuBar;
     }
+    private class saveTxt implements ActionListener {
 
-    /*@Override
-    public void actionPerformed(ActionEvent s) {
-        String txt = textPane1.getText();
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            final JFileChooser fc = new JFileChooser(getSaveLocation());
+            fc.setDialogTitle("Directory selection");
+            fc.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            fc.setDialogType(JFileChooser.SAVE_DIALOG);
 
-        try {
-            Files.write(Paths.get("my-file.txt"), txt.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
+            String txt = textPane1.getText();
+
+            try {
+                Files.write(Paths.get("my-file.txt"), txt.getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-    }*/
+    }
+
+    private File getSaveLocation() {
+        JFileChooser fc = new JFileChooser();
+        fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        int result = fc.showSaveDialog(null);
+
+        if (result == fc.APPROVE_OPTION) {
+            return fc.getSelectedFile();
+        } else {
+            return null;
+        }
+    }
 }
