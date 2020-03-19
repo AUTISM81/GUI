ed87bc553666424d3b359820687ff2b76321166e
AUTISM81 <54669344+AUTISM81@users.noreply.github.com>
2020-03-19 11:24:00 +0100

    Save and open function done, made textpane have a different font and fontsize.

--- src/ForfattareProgram.java
+++ src/ForfattareProgram.java
@@ -3,9 +3,12 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.print.PrinterAbortException;
 import java.io.*;
+import java.nio.Buffer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class ForfattareProgram {
     private JPanel panel1;
@@ -21,14 +24,19 @@ public class ForfattareProgram {
         frame.pack();
         frame.setVisible(true);
         frame.setJMenuBar(menuBar());
+        //gör så att de båda knapparna har olika funktioner
         save.addActionListener(new saveTxt());
+        open.addActionListener(new openTxt());
     }
 
     public static void main(String[] args) {
+
+        //startar uppbygnade av programmet
         new ForfattareProgram();
 
     }
 
+    //menubaren som ska sitta i paneln
     public JMenuBar menuBar() {
         JMenuBar menuBar;
         JMenu menu;
@@ -38,34 +46,73 @@ public class ForfattareProgram {
         save = new JMenuItem("save");
         open = new JMenuItem("open");
 
+        //lägger ihop allting till en hel meny
         menuBar.add(menu);
         menu.add(save);
         menu.add(open);
 
         return menuBar;
     }
+
+    //klass för att kunna spara all text som en txt fil
     private class saveTxt implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent actionEvent) {
-            final JFileChooser fc = new JFileChooser(getSaveLocation());
+            //frågar vad filen ska heta och sedan gör det till en txt
+            String fileName = name();
+
+            //tar all text för att kunna spara den.
             String txt = textPane1.getText();
 
-            try(FileWriter fw = new FileWriter(fc.getSelectedFile()+".txt")) {
-                fw.write(textPane1.toString());
+
+            try {
+                //sparar den som en txt fil
+                BufferedWriter bw = new BufferedWriter( new FileWriter(".\\" + fileName));
+
+                //bestämmer vad det är som sparas i txt fil
+                bw.write(txt);
+
+                //stänger denna metod
+                bw.close();
+            } catch (IOException e) {
+                //ifall något skulle vara fel körs denna kodsnitt
+                e.printStackTrace();
             }
         }
     }
 
-    private File getSaveLocation() {
-        JFileChooser fc = new JFileChooser();
-            fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-            int result = fc.showSaveDialog();
+    //öppnar programmet
+    private class openTxt implements ActionListener {
 
-            if (result == fc.APPROVE_OPTION) {
-            return fc.getSelectedFile();
-        } else {
-            return null;
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            //frågar vad filen ska heta och sedan gör det till en txt
+            String fileName = name();
+
+            //ifall det skulle
+            String text = null;
+            try {
+                //hittar vars filen är och tar den
+                Scanner file = new Scanner(new File(fileName));
+
+                while (file.hasNext()) {
+                    //läser in varje rad av filen så att det hamnar på rätt rad och inte all text på en rad när du öppnar
+                    text = file.next();
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+            //sätter texten från filen som har lästs in till det som displayas
+            textPane1.setText(text);
         }
+    }
+
+    public static String name() {
+        //frågar vad filen ska heta och sedan gör det till en txt
+        String fileName = JOptionPane.showInputDialog("What is the name of the file?");
+        fileName += ".txt";
+
+        return fileName;
     }
 }
